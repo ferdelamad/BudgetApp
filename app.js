f929cb78970fd7bc861ab463b72b89b52863d7ce
Fernando <ferdelamad@gmail.com>
2018-05-01 23:00:22 -0700

    added initial structure

---
+++ app.js
@@ -0,0 +1,11 @@
+var budgetController = (function() {
+  //private code here
+})();
+
+var uiController = (function() {
+  //private code here
+})();
+
+var controller = (function(budgetCtrl, uiCtrl) {
+  //private code here
+})(budgetController, uiController);
