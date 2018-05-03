3ab60f264f61ef35b05e6f6cdc7de2f08cea5a29
Fernando <ferdelamad@gmail.com>
2018-05-02 23:58:59 -0700

    updated event listeners

--- app.js
+++ app.js
@@ -12,18 +12,24 @@ var uiController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, uiCtrl) {
   //private code here
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    // 2. Add the item to the budgetController
+    // 3. Add the new item to the UI
+    // 4. Calculate the new budget
+    // 5. Display the budget on the UI
+  }
+
   document.querySelector('.add__btn').addEventListener('click', function () {
     console.log('This button was clicked!')
-     // 1. Get the field input data
-     // 2. Add the item to the budgetController
-     // 3. Add the new item to the UI
-     // 4. Calculate the new budget
-     // 5. Display the budget on the UI
 
   });
 
   document.addEventListener('keypress', function(event) {
-    console.log(event);
+
+    if (event.keyCode === 13 || event.which === 13) {
+      console.log('ENTER was pressed.');
+    }
   });
 
 })(budgetController, uiController);
