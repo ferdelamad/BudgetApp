f028bfabc36622c5b2941c79a9d5d3edb00591ae
Fernando <ferdelamad@gmail.com>
2018-05-03 01:08:05 -0700

    updated modules structure

--- app.js
+++ app.js
@@ -1,35 +1,62 @@
-// BUDGET CONTROLLER
-var budgetController = (function() {
-  //private code here
-})();
 
-// UI CONTROLLER
-var uiController = (function() {
-  //private code here
+//BUDGET CONTROLLER
+var budgetController = (function () {
+
+
 })();
 
 
-// GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, uiCtrl) {
-  //private code here
-  var ctrlAddItem = function() {
-    // 1. Get the field input data
-    // 2. Add the item to the budgetController
-    // 3. Add the new item to the UI
-    // 4. Calculate the new budget
-    // 5. Display the budget on the UI
+//UI CONTROLLER
+var uiController = (function(){
+
+  var domStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
   }
 
-  document.querySelector('.add__btn').addEventListener('click', function () {
-    console.log('This button was clicked!')
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(domStrings.inputType).value,  // Will be either inc or exp (+ o -)
+        description: document.querySelector(domStrings.inputDescription).value,
+        value: document.querySelector(domStrings.inputValue).value
+      };
+    },
 
-  });
+    getDOMstrings: function() {
+      return domStrings;
+    }
+
+  }
+
+})();
+
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, uiCtrl){
+
+   var DOM = uiController.getDOMstrings();
+
+   var ctrlAddItem = function() {
+     // 1. Get the field input data
+     var input = uiCtrl.getInput();
+     console.log(input);
+     // 2. Add the item to the budget controller
+     // 3. Add the item to the UI
+     // 4. Calculate the budget
+     // 5. Display the budget on the UI
+   };
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
 
-    if (event.keyCode === 13 || event.which === 13) {
-      console.log('ENTER was pressed.');
+    if (event.keyCode === 13  || event.which === 13) {
+      ctrlAddItem();
     }
+
   });
 
 })(budgetController, uiController);
