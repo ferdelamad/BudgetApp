cac3d3ec6946635efc618a5475dfe570723a9834
Fernando <ferdelamad@gmail.com>
2018-05-03 10:57:53 -0700

    added an initialization function for the app

--- app.js
+++ app.js
@@ -37,26 +37,36 @@ var uiController = (function(){
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, uiCtrl){
 
-   var DOM = uiController.getDOMstrings();
+   var setUpEventListeners = function() {
+     var DOM = uiCtrl.getDOMstrings();
+
+     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+     document.addEventListener('keypress', function(event) {
+       if (event.keyCode === 13  || event.which === 13) {
+         ctrlAddItem();
+       }
+     });
+   }
 
    var ctrlAddItem = function() {
      // 1. Get the field input data
      var input = uiCtrl.getInput();
-     console.log(input);
      // 2. Add the item to the budget controller
      // 3. Add the item to the UI
      // 4. Calculate the budget
      // 5. Display the budget on the UI
+     console.log(input);
    };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-
-    if (event.keyCode === 13  || event.which === 13) {
-      ctrlAddItem();
-    }
-
-  });
+   //return a PUBLIC initialization function to run the program
+   return {
+     init: function() {
+       console.log('The App started!')
+       setUpEventListeners();
+     }
+   };
 
 })(budgetController, uiController);
+
+controller.init();
