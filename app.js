4baf15126704519c2206fe027659aca22ce86e92
Fernando <ferdelamad@gmail.com>
2018-05-07 20:54:35 -0700

    added restrictions to the input fields

--- app.js
+++ app.js
@@ -148,17 +148,20 @@ var controller = (function(budgetCtrl, uiCtrl){
      var input, newItem;
      // 1. Get the field input data
      input = uiCtrl.getInput();
+
+     if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
      // 2. Add the item to the budget controller
      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
      // 3. Add the item to the UI
-     if(input.type && input.description.length){
      uiCtrl.addListItem(newItem, input.type);
-     }
+
      // 4. Clear the fields
      uiCtrl.clearFields();
      // 5. Calculate and display buget
 
      //Test -> console.log(input);
+
+    }
    };
 
 
